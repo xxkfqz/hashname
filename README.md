5b3ed700080ee94d47a30499fbe2711e90fedd55
xxkfqz <xxkfqz@gmail.com>
2026-07-26 12:58:13 +0500

    v0.2

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hashname
-Rename files to their hash
+Utility for renaming files to their SHA256 hash. It is mainly used to find and delete identical files within the same directory.
+
+## Examples
+
+```shell
+$ hashname ./data0.bin ./data1.bin ./data2.bin ./data3.bin
+$ ls
+0ac074949ad046dbd12d5be323cb0a9bcc9bb913d6f76defa57443a8c0eb64fc.bin
+dd01352a1c9ba7be8343bc149a8a9f70529349f6147c73648a6ca4941e124f6e.bin
+aeda128839e71ff642eadbeb6dff76829a80284986249840c4cf4235c47fa8ba.bin
+f18a23f874703d435516bdc6267d29ce93d7c2be48a2aaa1a9620600a99f0ae9.bin
+```
--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,77 @@
+use clap::Parser;
+use rayon::prelude::*;
+use sha2::{Digest as Sha2Digest, Sha256};
 use std::{
-    process::exit,
-    path::{
-        Path,
-        PathBuf,
-    },
-    fs,
-    result::Result,
     error::Error,
     ffi::OsStr,
+    fs,
+    io::{self, Read},
+    path::{Path, PathBuf},
+    result::Result,
 };
-use argparse::{ArgumentParser, StoreTrue, List};
-use sha256::try_digest;
-use rayon::prelude::*;
 
-struct GlobalOptions {
-    verbose: bool,
-    force_rehash: bool,
-    force_rename: bool,
-    dry_run: bool,
-    version: bool,
+#[derive(Parser)]
+#[command(name = "hashname", version, about = "Rename files to their hash")]
+struct Args {
+    /// Files to process
+    #[arg(num_args = 1..)]
     files: Vec<String>,
+
+    /// Do not actually rename files
+    #[arg(short, long)]
+    dry_run: bool,
+    /// Process the file even if its name looks like it has already been processed
+    #[arg(short, long)]
+    force_rehash: bool,
+    /// Rename/replace a file even there if another file with the same name exists
+    #[arg(short = 'F', long)]
+    force_rename: bool,
+    /// Explain what is being done
+    #[arg(short, long)]
+    verbose: bool,
 }
 
 fn main() {
-    let mut opts = GlobalOptions {
-        verbose: false,
-        force_rehash: false,
-        force_rename: false,
-        dry_run: false,
-        version: false,
-        files: vec![],
-    };
-    parse_args(&mut opts);
+    let opts = Args::parse();
 
-    if opts.version {
-        println!("{}", env!("CARGO_PKG_VERSION"));
-        exit(0);
-    }
-
-    opts.files.par_iter().for_each(
-        |x| {
-            match process_file(&opts, &x) {
-                Ok(result) => {
-                    if result.len() > 0 {
-                        println!("\"{}\" -> {:?}", x, result);
-                    }
-                }
-                Err(msg) => {
-                    if opts.verbose {
-                        eprintln!("Skipped \"{}\": {}", x, msg.to_string());
-                    }
+    opts.files
+        .par_iter()
+        .for_each(|x| match process_file(&opts, &x) {
+            Ok(result) => {
+                if opts.verbose && result.len() > 0 {
+                    println!("\"{}\" -> \"{}\"", x, result);
                 }
             }
-        }
-    );
+            Err(msg) => {
+                if opts.verbose {
+                    eprintln!("Skipped \"{}\": {}", x, msg.to_string());
+                }
+            }
+        });
 }
 
-fn parse_args(opts: &mut GlobalOptions) {
-    let mut ap = ArgumentParser::new();
-    ap.set_description("Rename files to their hash");
-    ap.refer(&mut opts.dry_run)
-        .add_option(
-            &["-d", "--dry-run"],
-            StoreTrue,
-            "Do not actually rename files",
-        );
-    ap.refer(&mut opts.force_rehash)
-        .add_option(
-            &["-f", "--force-rehash"],
-            StoreTrue,
-            "Process the file even if it looks like it has already been processed",
-        );
-    ap.refer(&mut opts.force_rename)
-        .add_option(
-            &["-F", "--force-rename"],
-            StoreTrue,
-            "Rename file even there is another file with the same result name",
-        );
-    ap.refer(&mut opts.verbose)
-        .add_option(
-            &["-v", "--verbose"],
-            StoreTrue,
-            "Print more information during processing",
-        );
-    ap.refer(&mut opts.version)
-        .add_option(
-            &["-V", "--version"],
-            StoreTrue,
-            "Print version and exit",
-        );
-    ap.refer(&mut opts.files)
-        .add_argument(
-            "file",
-            List,
-            "Files to process",
-        );
-    ap.parse_args_or_exit();
-}
-
-fn process_file(opts: &GlobalOptions, raw_filename: &String) -> Result<String, Box<dyn Error>> {
+fn process_file(opts: &Args, raw_filename: &String) -> Result<String, Box<dyn Error>> {
     let path_file = Path::new(raw_filename);
-    if fs::symlink_metadata(raw_filename)?.file_type().is_symlink() || !fs::metadata(raw_filename)?.file_type().is_file() {
+    if is_file(raw_filename)? {
         return Err(Box::from("Not a file"));
     }
 
     let file_name = get_str_from_osstr(&path_file.file_stem())?;
-
     if !opts.force_rehash && is_already_processed(&file_name) {
         return Err(Box::from("Already processed"));
     }
 
     let file_ext = get_str_from_osstr(&path_file.extension())?;
-    let result_hash = try_digest(path_file)?;
+    let result_hash = calculate_file_sha256(path_file)?;
     let result_filename = match file_ext.len() {
         0 => result_hash,
-        _ => format!("{}.{}", result_hash, file_ext)
+        _ => format!("{}.{}", result_hash, file_ext),
     };
-
     let mut path = PathBuf::from(path_file);
     path.set_file_name(result_filename.clone());
-
     let result_path = match path.into_os_string().into_string() {
         Ok(s) => s,
-        Err(_) => return Err(Box::from("Could not process filename"))
+        Err(_) => return Err(Box::from("Could not process filename")),
     };
 
     if !opts.force_rename && is_already_exists(&result_path) {
@@ -136,22 +85,45 @@ fn process_file(opts: &GlobalOptions, raw_filename: &String) -> Result<String, B
     Ok(result_path)
 }
 
+fn is_file(raw_filename: &String) -> Result<bool, Box<dyn Error>> {
+    Ok(fs::symlink_metadata(raw_filename)?.file_type().is_symlink()
+        || !fs::metadata(raw_filename)?.file_type().is_file())
+}
+
 fn get_str_from_osstr(osstr: &Option<&OsStr>) -> Result<String, Box<dyn Error>> {
-    Ok(
-        match osstr {
-            Some(n) => match n.to_str() {
-                Some(s) => s.to_string(),
-                None => return Err(Box::from("Could not get string from OsStr"))
-            },
-            None => return Err(Box::from("Could not get string from OsStr"))
-        }
-    )
+    Ok(match osstr {
+        Some(n) => match n.to_str() {
+            Some(s) => s.to_string(),
+            None => return Err(Box::from("Could not get string from OsStr")),
+        },
+        None => return Err(Box::from("Could not get string from OsStr")),
+    })
 }
 
 fn is_already_processed(filename: &String) -> bool {
-    filename.len() == 64 && filename.chars().all(|c| c.is_ascii_hexdigit())
+    filename.len() == 64
+        && filename
+            .chars()
+            .all(|c| c.is_ascii_digit() || (c.is_ascii_alphabetic() && c.is_ascii_lowercase()))
 }
 
 fn is_already_exists(filename: &String) -> bool {
     Path::new(filename).exists()
+}
+
+fn calculate_file_sha256(path: &Path) -> io::Result<String> {
+    let file = fs::File::open(path)?;
+    let mut reader = io::BufReader::new(file);
+    let mut hasher = Sha256::new();
+    let mut buffer = [0u8; 1048576]; // 1 MB
+
+    loop {
+        let bytes_read = reader.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        hasher.update(&buffer[..bytes_read]);
+    }
+
+    Ok(format!("{:x}", base16ct::HexDisplay(&hasher.finalize())))
 }
